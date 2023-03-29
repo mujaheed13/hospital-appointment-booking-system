e75a7b52e07bdc10231c056a4a20d48b3e8ab67e
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-29 13:46:59 +0530

    Appointment Schema

---
+++ models/appointment.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const appSchema = mongoose.Schema({
+    user_id: {
+        type: String,
+        required: true
+    },
+    doctor_id: {
+        type: String,
+        required: true
+    },
+    appointment_slot: {
+        type: Object,
+        required: true
+    }
+
+});
+
+const AppointmentModel = mongoose.model("appointment", appSchema);
+
+module.exports = { AppointmentModel }
---
+++ routes/appointment.route.js
@@ -0,0 +1,3 @@
+const { Router } = require("express");
+const appointmentRoute = Router();
+
