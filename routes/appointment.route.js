b8241150ed35d99fa45952c922f989bd903af113
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-29 22:47:44 +0530

    Delete Appointment

--- routes/appointment.route.js
+++ routes/appointment.route.js
@@ -1,32 +1,43 @@
 const { Router } = require("express");
 const { AppointmentModel } = require("../models/appointment.model");
-const appointmentRoute = Router(); 
+const appointmentRoute = Router();
 
-appointmentRoute.get("/", async (req, res)=>{
-    const user_id = req.query.user_id;
-    try {
-        if(user_id){
-            const appointments = await AppointmentModel.find({user_id});
-            res.send(appointments);
-            return;
-        }
-        const appointments = await AppointmentModel.find();
-        res.send(appointments);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({error_msg: error});
+appointmentRoute.get("/", async (req, res) => {
+  const user_id = req.query.user_id;
+  try {
+    if (user_id) {
+      const appointments = await AppointmentModel.find({ user_id });
+      res.send(appointments);
+      return;
     }
-})
+    const appointments = await AppointmentModel.find();
+    res.send(appointments);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error_msg: error });
+  }
+});
 
 appointmentRoute.post("/book", async (req, res) => {
+  try {
+    const appointment = AppointmentModel(req.body);
+    await appointment.save();
+    res.send({ msg: "Appointment Booked" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error_msg: error });
+  }
+});
+
+appointmentRoute.delete("/:id", async (req, res) => {
+    const { id } = req.params;
     try {
-        const appointment = AppointmentModel(req.body);
-        await appointment.save();
-        res.send({msg: "Appointment Booked"});
+        await AppointmentModel.deleteOne({_id: id});
+        res.send({msg: "Appointment Deleted"});
     } catch (error) {
         console.log(error);
-        res.status(500).send({error_msg: error});
+        res.status(500).send({ error_msg: error });
     }
-})
+});
 
-module.exports = { appointmentRoute }
+module.exports = { appointmentRoute };
