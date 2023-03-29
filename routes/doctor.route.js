b7d51100fa80544e4db32a9d94345bd0b3fdf096
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-30 00:07:34 +0530

    get Doctors

--- routes/doctor.route.js
+++ routes/doctor.route.js
@@ -1,18 +1,27 @@
 const { Router } = require("express");
 const { DoctorModel } = require("../models/doctor.model.js");
 const doctorRoute = Router();
-const slots = require("../utils/slots.js")
+const slots = require("../utils/slots.js");
 
-doctorRoute.post("/", async (req, res) => {
+doctorRoute.post("/add", async (req, res) => {
   try {
-    const doctor = DoctorModel({ ...req.body, slots});
+    const doctor = DoctorModel({ ...req.body, slots });
     await doctor.save();
-    res.send({msg: "Doctor added"});
+    res.send({ msg: "Doctor added" });
   } catch (error) {
     console.log(error);
     res.status(500).send({ error_msg: error });
   }
 });
 
+doctorRoute.get("/", async (req, res) => {
+  try {
+    const doctors = await DoctorModel.find();
+    res.send(doctors);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error_msg: error });
+  }
+});
 
 module.exports = { doctorRoute };
