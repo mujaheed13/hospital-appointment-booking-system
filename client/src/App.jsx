26569f3af7f153f26f3443678b1352b5ea25f3c9
Abhay Singh <abhay.jhonsmith@gmail.com>
2023-03-29 13:50:12 +0530

    Added All Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
+import { Route } from 'react-router-dom'
 
 import './App.css'
 import Footer from './components/Footer'
 import Midsection from './components/Midsection'
 import Navbar from './components/Navbar'
+import Login from './components/login'
+import AllRoute from './routes/route'
 
 function App() {
   
 
   return (
-    <div>
-      <Navbar />
-      <Midsection />
-      <Footer />
-    </div>
+    <>
+      <AllRoute />
+    </>
   )
 }
 
