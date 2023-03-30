3dffd27c3ee2f3a98b3d767f95c94558a86ed54a
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-30 14:24:08 +0530

    updated booking

--- middleware/available-slot.js
+++ middleware/available-slot.js
@@ -1,16 +1,21 @@
 const { DoctorModel } = require("../models/doctor.model.js");
+
 const is_slot_available = async (req, res, next) => {
   let { day, doctor } = req.params;
+
   doctor = doctor.toLowerCase();
   day = day.toLowerCase();
-  console.log(day, doctor);
+
   const data = await DoctorModel.findOne({ name: doctor });
-  data?.slots?.forEach((el) => {
+
+  data?.slots?.forEach( async (el, i) => {
     if (el.day == day && el.is_booked == false) {
+      await DoctorModel.updateOne({_id: data._id, slots: {$elemMatch: {day: day}}}, {$set:{"slots.$.is_booked": true}})
+
       next();
-      console.log(111);
       return;
     }
+
     if (el.day == day && el.is_booked == true) {
       res.send({ msg: "Slot is already booked" });
       return;
