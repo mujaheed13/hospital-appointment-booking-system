de44a6ae42168b191c4677325c2042f3ec816cc5
Abhay Singh <abhay.jhonsmith@gmail.com>
2023-04-01 19:40:36 +0530

    Created User and Admin Dashboard component and Connected Api's

---
+++ client/src/pages/login/LoginForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from "../../components/Footer";
+import Login from "../../components/login";
+import Navbar from "../../components/Navbar";
+const LoginForm = () => {
+  return (
+    <>
+      <Navbar />
+      <Login/>
+      <Footer />
+    </>
+  )
+}
+
+export default LoginForm
--- config/googleOAuth.js
+++ config/googleOAuth.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8080/appointments/google/auth/callback",
+      callbackURL: "https://lifecare-mwbk.onrender.com/appointments/google/auth/callback",
     },
     async (accessToken, refreshToken, profile, cb) => {
       let email = profile._json.email;
